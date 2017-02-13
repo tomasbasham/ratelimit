ef53ab1020d1adf4576dec1de4ae30d1ce86865e
Tomas Basham <tomasbasham@gmail.com>
2017-02-13 16:20:32 +0000

    Make the usage documentation more clear

--- tests/unit/decorator_test.py
+++ tests/unit/decorator_test.py
@@ -1,9 +1,20 @@
-from ratelimit import *
+from ratelimit import rate_limited
 from tests import *
 
 class TestDecorator(unittest.TestCase):
 
+  @rate_limited(1, 2)
+  def increment(self):
+    '''
+    Increment the counter at most once
+    every 2 seconds.
+    '''
+    self.count += 1
+
+  def setUp(self):
+    self.count = 0
+
   def test_decorator(self):
-    limiter = rate_limited(1)
-    decorated = limiter(lambda: 'None')
-    self.assertEqual(decorated(), 'None')
+    self.assertEqual(self.count, 0)
+    self.increment()
+    self.assertEqual(self.count, 1)
