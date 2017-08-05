8b33108dc676f8e47ca879270fe52cacb9e4259d
Tomas Basham <tomasbasham@gmail.com>
2017-08-05 15:53:33 +0100

    Lint package to conform to Python style guides

--- setup.py
+++ setup.py
@@ -2,27 +2,34 @@ from setuptools import setup
 from ratelimit.version import Version
 
 def readme():
-  with open('README.rst') as file:
-    return file.read()
+    '''Read README file'''
+    with open('README.rst') as infile:
+        return infile.read()
 
-setup(name='ratelimit',
-      version=Version('1.3.0').number,
-      description='API rate limit decorator',
-      long_description=readme().strip(),
-      author='Tomas Basham',
-      author_email='me@tomasbasham.co.uk',
-      url='https://github.com/tomasbasham/ratelimit',
-      license='MIT',
-      packages=['ratelimit'],
-      install_requires=[],
-      keywords='ratelimit api decorator',
-      classifiers=[
+setup(
+    name='ratelimit',
+    version=Version('1.3.0').number,
+    description='API rate limit decorator',
+    long_description=readme().strip(),
+    author='Tomas Basham',
+    author_email='me@tomasbasham.co.uk',
+    url='https://github.com/tomasbasham/ratelimit',
+    license='MIT',
+    packages=['ratelimit'],
+    install_requires=[],
+    keywords=[
+        'ratelimit',
+        'api',
+        'decorator'
+    ],
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python',
         'Topic :: Software Development'
-      ],
-      include_package_data=True,
-      zip_safe=False)
+    ],
+    include_package_data=True,
+    ip_safe=False
+)
