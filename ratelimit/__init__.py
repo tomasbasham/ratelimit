ba91e77c0c1f4ac6650578107cf4907b97beb0c5
Tomas Basham <tomasbasham@gmail.com>
2018-04-26 21:21:26 +0100

    Alias `limits` for backwards compatibility

--- ratelimit/__init__.py
+++ ratelimit/__init__.py
@@ -12,10 +12,12 @@ from ratelimit.decorators import RateLimitDecorator, sleep_and_retry
 from ratelimit.exception import RateLimitException
 
 limits = RateLimitDecorator
+rate_limited = RateLimitDecorator # For backwards compatibility
 
 __all__ = [
     'RateLimitException',
     'limits',
+    'rate_limited',
     'sleep_and_retry'
 ]
 
