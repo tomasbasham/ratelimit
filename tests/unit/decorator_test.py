19a4d175ae00c76aad5a027d3a52d61c506a2d2a
Jordan Zwan <jordan@remin.no>
2022-06-15 15:58:00 +0200

    added treshold tests for soft-limit functionalty

--- tests/unit/decorator_test.py
+++ tests/unit/decorator_test.py
@@ -1,7 +1,10 @@
+from unittest.mock import MagicMock
 from ratelimit import limits, RateLimitException
 from tests import unittest, clock
 
+
 class TestDecorator(unittest.TestCase):
+    treshold_method = MagicMock()
 
     @limits(calls=1, period=10, clock=clock)
     def increment(self):
@@ -13,12 +16,43 @@ class TestDecorator(unittest.TestCase):
     @limits(calls=1, period=10, clock=clock, raise_on_limit=False)
     def increment_no_exception(self):
         '''
-        Increment the counter at most once every 10 seconds, but w/o rasing an
-        exception when reaching limit.
+        Increment the counter at most once every 10 seconds, without raising an exception when
+        reaching the rate limit.
+        '''
+        self.count += 1
+
+    @limits(
+        calls=1,
+        period=10,
+        clock=clock,
+        raise_on_limit=False,
+        treshold=0.5,
+        treshold_method=treshold_method
+    )
+    def increment_treshold_no_exception(self):
+        '''
+        Increment the counter at most once every 10 seconds, invoking the treshold method when
+        the treshold is reached, without raising an exception when reaching the rate limit.
+        '''
+        self.count += 1
+
+    @limits(
+        calls=4,
+        period=10,
+        clock=clock,
+        raise_on_limit=True,
+        treshold=0.5,
+        treshold_method=treshold_method
+    )
+    def increment_treshold(self):
+        '''
+        Increment the counter at most once every 10 seconds, triggering the treshold method when
+        the treshold is reached, while raising an exception when reaching the rate limit.
         '''
         self.count += 1
 
     def setUp(self):
+        self.treshold_method.reset_mock()
         self.count = 0
         clock.increment(10)
 
@@ -42,3 +76,29 @@ class TestDecorator(unittest.TestCase):
         self.increment_no_exception()
 
         self.assertEqual(self.count, 1)
+
+    def test_treshold_no_exception(self):
+        self.increment_treshold_no_exception()
+        self.increment_treshold_no_exception()
+        self.treshold_method.assert_called_once()
+
+    def test_treshold(self):
+        # These first 3 calls should not trigger the treshold method nor hit the rate limit
+        self.increment_treshold()
+        self.treshold_method.assert_not_called()
+
+        self.increment_treshold()
+        self.treshold_method.assert_not_called()
+
+        self.increment_treshold()
+        self.treshold_method.assert_not_called()
+
+        # This call causes the allowance to drop under the treshold, triggering the treshold_method
+        self.increment_treshold()
+        self.treshold_method.assert_called_once()
+
+        # This call is made while the allowance is still under the treshold value, triggering the
+        # treshold_method once more while also blocking the request as the rate limit has been hit
+        self.treshold_method.reset_mock()
+        self.assertRaises(RateLimitException, self.increment_treshold)
+        self.treshold_method.assert_called_once()
